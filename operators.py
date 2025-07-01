ec906a1043b829a3b36533d477e3f0497629b06a
divya-ponnusamy <pdivyajob@gmail.com>
2025-07-01 15:38:33 +0530

    Add operators file

---
+++ operators.py
@@ -0,0 +1,37 @@
+'''-----Arithmetic operators--------
+
+a=int(input("Enter your first value:"))
+b=int(input("Enter your Second value:"))
+print(a+b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a*b)
+print(a**b)
+#-------comparison operator-------
+a=int(input("enter your first value:"))
+b=int(input("enter your second value:"))
+
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+-------logical operator-------
+
+a=10
+b=8
+c=12
+
+print(a>b and b<c)
+
+print(a>b or b>c)
+
+print(not(a>c or c>b))
+
+
+-------Assignment operator-------
+'''
