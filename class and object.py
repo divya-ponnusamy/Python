29c5900a058d25e7765b0f90b3a3eda25f31737d
divya-ponnusamy <pdivyajob@gmail.com>
2025-07-14 21:16:24 +0530

    Practices class and objects

---
+++ class and object.py
@@ -0,0 +1,77 @@
+'''class laptop:
+    price=0
+    ram=""
+    processer=""
+    def display(self):
+        print("disply")
+hp=laptop()
+Del=laptop()
+lenova=laptop()
+hp.price=50000
+hp.ram="8gb"
+hp.processer="i6"
+print(hp.price)
+ob=laptop()
+ob.display()
+Del.price=20000
+Del.ram="7gb"
+Del.processer="i6"
+print(Del.ram)
+lenova.price=30000
+lenova.processer="i9"
+lenova.ram="7gb"
+print(lenova.ram)
+
+class laptop:
+    def __init__(self):
+        print("hai")
+    def display(self):
+        print("hello")
+ob=laptop()
+ob.display()
+'''
+'''class laptop():
+    ram=""
+    price=""
+    def display(self):
+        print("display")
+hp=laptop()
+hp.ram="8gb"
+hp.price="30000"
+print(hp.ram)
+print(hp.price)'''
+
+'''
+class laptop():
+    def __init__(self):
+        print("hai")
+dell=laptop()
+'''
+'''
+class laptop():
+    def __init__(self):
+        self.ram=""
+        self.price=""
+    def display(self):
+        print("Laptop ram:",self.ram)
+        print("Laptop price:",self.price)
+dell=laptop()
+dell.ram="6gb"
+dell.price="90000"
+dell.display()
+
+lenova=laptop()
+lenova.ram="7gb"
+lenova.price="10000000"
+lenova.display()
+
+class laptop():
+    def __init__(self,ram,price):
+        self.ram=ram
+        self.price=price
+    def display(self):
+        print("Laptop ram:",self.ram)
+        print("Laptop price:",self.price)
+dell=laptop("9gb","1234566")
+dell.display()
+ '''                
